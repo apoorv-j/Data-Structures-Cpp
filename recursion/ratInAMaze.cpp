0e9359c8d87b3619718e1c66b3b89740c3a47af7
apoorv-j <jainapoorv341@gmail.com>
2020-06-10 23:47:15 +0530

    rat in a maze added

--- recursion/ratInAMaze.cpp
+++ recursion/ratInAMaze.cpp
@@ -1,47 +1,91 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void moveRat(char **arr, bool **store, int i, int j, int n, int m)
+void printAllPaths(char **arr, bool **store, int i, int j, int n, int m)
 {
     store[i][j] = 1;
     if (i == n and j == m)
     {
+        cout << endl;
+
         for (int i = 0; i <= n; i++)
         {
             for (int j = 0; j <= m; j++)
             {
-                if (store[i][j])
-                    cout << "(" << i << "," << j << ") ";
+                cout << store[i][j] << " ";
             }
+            cout << endl;
         }
-        cout << endl;
         store[i][j] = 0;
         return;
     }
-
     //move right
     if (j < m and arr[i][j + 1] == 'O')
     {
-        moveRat(arr, store, i, j + 1, n, m);
+        printAllPaths(arr, store, i, j + 1, n, m);
     }
-
     //move down
     if (i < n and arr[i + 1][j] == 'O')
     {
-        moveRat(arr, store, i + 1, j, n, m);
+        printAllPaths(arr, store, i + 1, j, n, m);
     }
-
     //move left
     if (i > 0 and arr[i - 1][j] == '0')
     {
-        moveRat(arr, store, i - 1, j, n, m);
+        printAllPaths(arr, store, i - 1, j, n, m);
     }
     //move up
     if (j > 0 and arr[i][j - 1] == '0')
     {
-        moveRat(arr, store, i, j - 1, n, m);
+        printAllPaths(arr, store, i, j - 1, n, m);
     }
     store[i][j] = 0;
+}
+
+bool moveRat(char **arr, bool **store, int i, int j, int n, int m)
+{
+    store[i][j] = 1;
+    if (i == n and j == m)
+    {
+        // cout << endl;
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= m; j++)
+            {
+                cout << store[i][j] << " ";
+            }
+            cout << endl;
+        }
+        store[i][j] = 0;
+        return true;
+    }
+    bool rp = false;
+    //move right
+    if (j < m and arr[i][j + 1] == 'O')
+    {
+        if (moveRat(arr, store, i, j + 1, n, m))
+            return true;
+    }
+    //move down
+    if (i < n and arr[i + 1][j] == 'O')
+    {
+        if (moveRat(arr, store, i + 1, j, n, m))
+            return true;
+    }
+    //move left
+    if (i > 0 and arr[i - 1][j] == '0')
+    {
+        if (moveRat(arr, store, i - 1, j, n, m))
+            return true;
+    }
+    //move up
+    if (j > 0 and arr[i][j - 1] == '0')
+    {
+        if (moveRat(arr, store, i, j - 1, n, m))
+            return true;
+    }
+    store[i][j] = 0;
+    return false;
 }
 
 int main()
@@ -63,7 +107,8 @@ int main()
         }
     }
 
-    moveRat(arr, store, 0, 0, n - 1, m - 1);
+    if (!moveRat(arr, store, 0, 0, n - 1, m - 1))
+        cout << -1;
 
     return 0;
 }
