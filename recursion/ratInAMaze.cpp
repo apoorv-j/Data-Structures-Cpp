51c1168b7216fab585f0d1828d7c6262ddc7cbe0
apoorv-j <jainapoorv341@gmail.com>
2020-06-14 23:18:37 +0530

    rat in a maze update

--- recursion/ratInAMaze.cpp
+++ recursion/ratInAMaze.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 bool moveRat(char **arr, bool **store, int i, int j, int n, int m)
 {
-    store[i][j] = 1;
-    if (i == n and j == m)
+    if (i == n - 1 and j == m - 1)
     {
+        store[i][j] = 1;
         // cout << endl;
-        for (int i = 0; i <= n; i++)
+        for (int i = 0; i < n; i++)
         {
-            for (int j = 0; j <= m; j++)
+            for (int j = 0; j < m; j++)
             {
                 cout << store[i][j] << " ";
             }
@@ -20,31 +20,28 @@ bool moveRat(char **arr, bool **store, int i, int j, int n, int m)
         // to print all paths
         // return false
     }
-    bool rp = false;
+
+    if (i < 0 or j < 0 or i > (n - 1) or j > (m - 1) or arr[i][j] == 'X' or store[i][j] == 1)
+        return false;
+
+    store[i][j] = 1;
+
     //move right
-    if (j < m and arr[i][j + 1] == 'O')
-    {
-        if (moveRat(arr, store, i, j + 1, n, m))
-            return true;
-    }
+    if (moveRat(arr, store, i, j + 1, n, m))
+        return true;
+
     //move down
-    if (i < n and arr[i + 1][j] == 'O')
-    {
-        if (moveRat(arr, store, i + 1, j, n, m))
-            return true;
-    }
-    //move left
-    if (i > 0 and arr[i - 1][j] == '0')
-    {
-        if (moveRat(arr, store, i - 1, j, n, m))
-            return true;
-    }
+    if (moveRat(arr, store, i + 1, j, n, m))
+        return true;
+
     //move up
-    if (j > 0 and arr[i][j - 1] == '0')
-    {
-        if (moveRat(arr, store, i, j - 1, n, m))
-            return true;
-    }
+    if (moveRat(arr, store, i - 1, j, n, m))
+        return true;
+
+    //move left
+    if (moveRat(arr, store, i, j - 1, n, m))
+        return true;
+
     store[i][j] = 0;
     return false;
 }
@@ -68,8 +65,8 @@ int main()
         }
     }
 
-    if (!moveRat(arr, store, 0, 0, n - 1, m - 1))
-        cout << -1;
+    if (!moveRat(arr, store, 0, 0, n, m))
+        cout << "NO PATH FOUND";
 
     return 0;
 }
